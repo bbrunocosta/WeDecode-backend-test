10d27d13b6628ceef97c86229aaf80268eb6f20b
Bruno Costa <b.brunocosta@outlook.com>
2021-07-26 15:22:06 -0300

    feat: ensure AddWatchedFilmController works as expected

---
+++ src/presentation/controllers/spectators/addWatchedFilm.controller.ts
@@ -0,0 +1,24 @@
+import { SpectatorRepository } from '../../../data/protocols/spectators/spectator.repository.prototol'
+import InternalServerError from '../../errors/internalServerError'
+import SpectatorNotFoundError from '../../errors/spectatorNotFoundError'
+import { badRequest, noContent, serverError } from '../../helpers/httpResponse.helper'
+import { HttpRequest, HttpResponse } from '../../protocols/http.protocol'
+
+class AddWatchedFilmController {
+  constructor (private readonly spectatorRepository: SpectatorRepository) {}
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const { id } = httpRequest.params
+      const { filmId } = httpRequest.body
+
+      await this.spectatorRepository.addWatchedFilm(id, filmId)
+
+      return noContent({})
+    } catch (err) {
+      if (err instanceof SpectatorNotFoundError) return badRequest(err)
+
+      return serverError(new InternalServerError(err.stack))
+    }
+  }
+}
+export default AddWatchedFilmController
