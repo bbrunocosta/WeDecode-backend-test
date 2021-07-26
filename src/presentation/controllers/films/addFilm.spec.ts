2e3e1c3576a342fdb79ada829f4850d3591748a3
Bruno Costa <b.brunocosta@outlook.com>
2021-07-26 12:17:27 -0300

    test: ensure returns 409 if film title aready exists

---
+++ src/presentation/controllers/films/addFilm.spec.ts
@@ -0,0 +1,40 @@
+import { FilmRepository } from '../../../data/protocols/films/film.repository.protocol'
+import { Film } from '../../../domain/film'
+import FilmTitleAlreadyExistsError from '../../errors/filmTitleAlreadyExistsError'
+import AddFilmController from './addFilm.controller'
+
+const fakeFilmData: Film = {
+  id: 'a valid id',
+  title: 'a valid title',
+  author: 'a valid author',
+  director: 'a valid director',
+  'created-at': 'valid timestamp'
+}
+const fakeHttpRequest = {
+  body: {
+    title: 'a valid title',
+    author: 'a valid author',
+    director: 'a valid director'
+  }
+}
+class AddFilmRepositoryStub implements FilmRepository {
+  async addFilm ({ title, author, director }: Omit<Film, 'id' | 'created-at'>): Promise<Film> {
+    return fakeFilmData
+  }
+
+  async findOneByTitle (title: string): Promise<Film | null> {
+    return null
+  }
+}
+describe('AddFilm', () => {
+  const addFilmRespoitoryStub = new AddFilmRepositoryStub()
+  const addFilmController = new AddFilmController(addFilmRespoitoryStub)
+  test('Sould return 409 if film title already exists', async () => {
+    const findOneByTitleSpy = jest.spyOn(addFilmRespoitoryStub, 'findOneByTitle')
+    findOneByTitleSpy.mockReturnValueOnce(Promise.resolve(fakeFilmData))
+    const httpResponse = await addFilmController.handle(fakeHttpRequest)
+    expect(findOneByTitleSpy).toBeCalledWith(fakeHttpRequest.body.title)
+    expect(httpResponse.status).toBe(409)
+    expect(httpResponse.body).toEqual(new FilmTitleAlreadyExistsError(fakeHttpRequest.body.title))
+  })
+})
