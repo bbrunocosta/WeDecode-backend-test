6807d765a895bd0426983f21de55c44f5dc9c528
Bruno Costa <b.brunocosta@outlook.com>
2021-07-26 12:50:19 -0300

    feat: emsure returns 500 if FilmRepository throws error

--- src/presentation/controllers/films/addFilm.controller.ts
+++ src/presentation/controllers/films/addFilm.controller.ts
@@ -1,19 +1,24 @@
 import { FilmRepository } from '../../../data/protocols/films/film.repository.protocol'
 import FilmTitleAlreadyExistsError from '../../errors/filmTitleAlreadyExistsError'
-import { conflict, created } from '../../helpers/httpResponse.helper'
+import InternalServerError from '../../errors/internalServerError'
+import { conflict, created, serverError } from '../../helpers/httpResponse.helper'
 import { HttpRequest, HttpResponse } from '../../protocols/http.protocol'
 
 class AddFilmController {
   constructor (private readonly filmRepository: FilmRepository) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { title, author, director } = httpRequest.body
+    try {
+      const { title, author, director } = httpRequest.body
 
-    const filmExists = await this.filmRepository.findOneByTitle(title)
+      const filmExists = await this.filmRepository.findOneByTitle(title)
 
-    if (filmExists) return conflict(new FilmTitleAlreadyExistsError(title))
+      if (filmExists) return conflict(new FilmTitleAlreadyExistsError(title))
 
-    const newFilm = await this.filmRepository.addFilm({ title, author, director })
-    return created(newFilm)
+      const newFilm = await this.filmRepository.addFilm({ title, author, director })
+      return created(newFilm)
+    } catch (err) {
+      return serverError(new InternalServerError(err.stack))
+    }
   }
 }
 export default AddFilmController
