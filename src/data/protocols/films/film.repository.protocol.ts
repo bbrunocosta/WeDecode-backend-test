c9d9e6f76f2e7f5dca2f18fc9244652cb65cedb0
Bruno Costa <b.brunocosta@outlook.com>
2021-07-26 13:14:51 -0300

    feat: add getAll function

--- src/data/protocols/films/film.repository.protocol.ts
+++ src/data/protocols/films/film.repository.protocol.ts
@@ -2,5 +2,6 @@ import { Film } from '../../../domain/film'
 
 export interface FilmRepository{
   addFilm: ({ title, author, director }: Omit<Film, 'id' | 'created-at'>) => Promise<Film>
+  getAll: () => Promise<Film[] | []>
   findOneByTitle: (title: string) => Promise<Film | null>
 }
