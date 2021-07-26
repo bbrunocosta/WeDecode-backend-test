dd709519dba1c441bc1f1e97c9d65c8d94bfee82
Bruno Costa <b.brunocosta@outlook.com>
2021-07-26 12:50:27 -0300

    test: emsure returns 500 if FilmRepository throws error

--- src/presentation/controllers/films/addFilm.spec.ts
+++ src/presentation/controllers/films/addFilm.spec.ts
@@ -1,6 +1,7 @@
 import { FilmRepository } from '../../../data/protocols/films/film.repository.protocol'
 import { Film } from '../../../domain/film'
 import FilmTitleAlreadyExistsError from '../../errors/filmTitleAlreadyExistsError'
+import InternalServerError from '../../errors/internalServerError'
 import AddFilmController from './addFilm.controller'
 
 const fakeFilmData: Film = {
@@ -17,7 +18,14 @@ const fakeHttpRequest = {
     director: 'a valid director'
   }
 }
-class AddFilmRepositoryStub implements FilmRepository {
+const fakeStack = 'a custom stack'
+class FakeErrorStub extends Error {
+  constructor () {
+    super()
+    this.stack = fakeStack
+  }
+}
+class FilmRepositoryStub implements FilmRepository {
   async addFilm ({ title, author, director }: Omit<Film, 'id' | 'created-at'>): Promise<Film> {
     return fakeFilmData
   }
@@ -27,10 +35,10 @@ class AddFilmRepositoryStub implements FilmRepository {
   }
 }
 describe('AddFilm', () => {
-  const addFilmRespoitoryStub = new AddFilmRepositoryStub()
-  const addFilmController = new AddFilmController(addFilmRespoitoryStub)
+  const filmRespoitoryStub = new FilmRepositoryStub()
+  const addFilmController = new AddFilmController(filmRespoitoryStub)
   test('Sould return 409 if film title already exists', async () => {
-    const findOneByTitleSpy = jest.spyOn(addFilmRespoitoryStub, 'findOneByTitle')
+    const findOneByTitleSpy = jest.spyOn(filmRespoitoryStub, 'findOneByTitle')
     findOneByTitleSpy.mockReturnValueOnce(Promise.resolve(fakeFilmData))
     const httpResponse = await addFilmController.handle(fakeHttpRequest)
     expect(findOneByTitleSpy).toBeCalledWith(fakeHttpRequest.body.title)
@@ -38,10 +46,16 @@ describe('AddFilm', () => {
     expect(httpResponse.body).toEqual(new FilmTitleAlreadyExistsError(fakeHttpRequest.body.title))
   })
   test('Sould return 201 on success', async () => {
-    const addFilmSpy = jest.spyOn(addFilmRespoitoryStub, 'addFilm')
+    const addFilmSpy = jest.spyOn(filmRespoitoryStub, 'addFilm')
     const httpResponse = await addFilmController.handle(fakeHttpRequest)
     expect(addFilmSpy).toBeCalledWith(fakeHttpRequest.body)
     expect(httpResponse.status).toBe(201)
     expect(httpResponse.body).toEqual(fakeFilmData)
+  })
+  test('Sould return 500 if FilmRepository throws', async () => {
+    jest.spyOn(filmRespoitoryStub, 'addFilm').mockImplementationOnce(() => { throw new FakeErrorStub() })
+    const httpResponse = await addFilmController.handle(fakeHttpRequest)
+    expect(httpResponse.status).toBe(500)
+    expect(httpResponse.body).toEqual(new InternalServerError(fakeStack))
   })
 })
