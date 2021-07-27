06f73e7b1689dc3e85dbfd6ce31c58c0cb88347b
Bruno Costa <b.brunocosta@outlook.com>
2021-07-27 04:40:55 -0300

    test: ensure calls validate with correct values

--- src/presentation/controllers/films/listViwersAmount.controller.spec.ts
+++ src/presentation/controllers/films/listViwersAmount.controller.spec.ts
@@ -1,6 +1,6 @@
 import FilmNotFoundError from '../../errors/filmNotFoundError'
 import InternalServerError from '../../errors/internalServerError'
-import { FakeErrorStub, fakeFilmData, fakeStack, FilmRepositoryStub } from './filmMocks'
+import { FakeErrorStub, fakeFilmData, fakeStack, FilmRepositoryStub, ValidationStub } from './filmMocks'
 import ListViwersAmountController from './listViwersAmount.controller'
 describe('ListFilms', () => {
   const fakeHttpRequest = {
@@ -9,13 +9,14 @@ describe('ListFilms', () => {
     }
   }
   const filmRepositoryStub = new FilmRepositoryStub()
-  const listfilms = new ListViwersAmountController(filmRepositoryStub)
+  const validationStub = new ValidationStub()
+  const listfilms = new ListViwersAmountController(filmRepositoryStub, validationStub)
   test('Should return 200 on success', async () => {
     const findOneByIdSpy = jest.spyOn(filmRepositoryStub, 'findOneById')
     const httpResponse = await listfilms.handle(fakeHttpRequest)
     expect(findOneByIdSpy).toHaveBeenCalledWith(fakeHttpRequest.params.id)
     expect(httpResponse.status).toBe(200)
-    expect(httpResponse.body).toEqual({ viewersAmount: fakeFilmData.viewersAmount })
+    expect(httpResponse.body).toEqual({ spectatorsAmount: fakeFilmData.viewersAmount, spectators: fakeFilmData.spectators })
   })
   test('Should return 400 if film was not found', async () => {
     jest.spyOn(filmRepositoryStub, 'findOneById').mockReturnValueOnce(Promise.resolve(null))
@@ -28,5 +29,10 @@ describe('ListFilms', () => {
     const httpResponse = await listfilms.handle(fakeHttpRequest)
     expect(httpResponse.status).toBe(500)
     expect(httpResponse.body).toEqual(new InternalServerError(fakeStack))
+  })
+  test('sould call validate with correct values', async () => {
+    const validateSpy = jest.spyOn(validationStub, 'validate')
+    await listfilms.handle(fakeHttpRequest)
+    expect(validateSpy).toHaveBeenCalledWith(fakeHttpRequest.params)
   })
 })
