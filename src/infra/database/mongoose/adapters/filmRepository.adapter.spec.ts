d9879049ae0dc33c601d1defbb7503626ac89901
Bruno Costa <b.brunocosta@outlook.com>
2021-07-26 23:22:59 -0300

    test: ensure addFilm returns a film on sucess

---
+++ src/infra/database/mongoose/adapters/filmRepository.adapter.spec.ts
@@ -0,0 +1,31 @@
+import MongooseFilmRepositoryAdapter from './filmRepository.adapter'
+import MongooseHelper from '../mongoose.Helper'
+import env from '../../../../main/config/env'
+describe('MongooseFilmRepositoryAdapter', () => {
+  beforeAll(async () => {
+    await MongooseHelper.connect(env.mongoUri)
+  })
+  afterAll(async () => {
+    await MongooseHelper.disconnect()
+  })
+  beforeEach(async () => {
+    await MongooseHelper.dropCollection('films')
+  })
+  const fakeFilmRequest1 = {
+    title: 'a tile',
+    author: 'an author',
+    director: 'a director'
+  }
+  const fakeFilmRequest2 = {
+    title: 'a tile2',
+    author: 'an author2',
+    director: 'a director2'
+  }
+  const filmRepository = new MongooseFilmRepositoryAdapter()
+  test('addFilm should return a film on sucess', async () => {
+    const result = await filmRepository.addFilm(fakeFilmRequest1)
+    expect(result.title).toBe(fakeFilmRequest1.title)
+    expect(result.author).toBe(fakeFilmRequest1.author)
+    expect(result.director).toBe(fakeFilmRequest1.director)
+  })
+})
