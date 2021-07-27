6b322aca0620546569a4498d97192a8940f76b13
Bruno Costa <b.brunocosta@outlook.com>
2021-07-27 04:26:23 -0300

    feat: add validationComposite and fix controller routine

--- src/presentation/controllers/spectators/listWatchedFilms.controller.ts
+++ src/presentation/controllers/spectators/listWatchedFilms.controller.ts
@@ -2,17 +2,30 @@ import { SpectatorRepository } from '../../../data/protocols/spectators/spectato
 import InternalServerError from '../../errors/internalServerError'
 import SpectatorNotFoundError from '../../errors/spectatorNotFoundError'
 import { badRequest, serverError, success } from '../../helpers/httpResponse.helper'
+import { Validator } from '../../helpers/validator/validator.protocol'
 import { Controller } from '../../protocols/controller.protocol'
 import { HttpRequest, HttpResponse } from '../../protocols/http.protocol'
 
 class ListWatchedFilmsController implements Controller {
-  constructor (private readonly spectatorRepository: SpectatorRepository) {}
+  constructor (
+    private readonly spectatorRepository: SpectatorRepository,
+    private readonly validationComposite: Validator
+  ) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const hasError = this.validationComposite.validate(httpRequest.params)
+      if (hasError) return badRequest(hasError)
+
       const { id } = httpRequest.params
+
       const spectator = await this.spectatorRepository.findOneById(id)
       if (!spectator) return badRequest(new SpectatorNotFoundError(id))
-      return success(spectator.whatchedFilms)
+
+      return success({
+        whatchedFilmsAmount: spectator.whatchedFilms.length,
+        whatchedFilms: spectator.whatchedFilms
+      })
     } catch (err) {
       return serverError(new InternalServerError(err.stack))
     }
