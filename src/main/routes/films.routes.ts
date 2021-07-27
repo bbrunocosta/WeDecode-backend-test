fb4b080510aa5ae328b66265923d326acd536234
Bruno Costa <b.brunocosta@outlook.com>
2021-07-26 23:55:22 -0300

    feat: add   films routes

---
+++ src/main/routes/films.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express'
+import { adaptRoute } from '../adapters/express-routes.adapter'
+import { makeAddFilmController } from '../factories/makeAddFilmController.factory'
+import { makeListFilmsController } from '../factories/makeListFilmsController.factory'
+import { makeListViwersAmountController } from '../factories/makeListViwersAmountController.factory'
+
+export default (router: Router): void => {
+  router.post('/films', adaptRoute(makeAddFilmController()))
+  router.get('/films', adaptRoute(makeListFilmsController()))
+  router.get('/films/viewers-amount/:id', adaptRoute(makeListViwersAmountController()))
+}
