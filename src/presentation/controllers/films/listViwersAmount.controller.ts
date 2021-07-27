bf8e93dcf4ad94cf646a800a5de08b2f933f32e8
Bruno Costa <b.brunocosta@outlook.com>
2021-07-27 04:40:23 -0300

    feat: add validation and fix sucess response

--- src/presentation/controllers/films/listViwersAmount.controller.ts
+++ src/presentation/controllers/films/listViwersAmount.controller.ts
@@ -2,18 +2,30 @@ import { FilmRepository } from '../../../data/protocols/films/film.repository.pr
 import FilmNotFoundError from '../../errors/filmNotFoundError'
 import InternalServerError from '../../errors/internalServerError'
 import { badRequest, serverError, success } from '../../helpers/httpResponse.helper'
+import { Validator } from '../../helpers/validator/validator.protocol'
 import { Controller } from '../../protocols/controller.protocol'
 import { HttpRequest, HttpResponse } from '../../protocols/http.protocol'
 
 class ListViwersAmountController implements Controller {
-  constructor(private readonly filmRepository: FilmRepository) { }
-  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+  constructor (
+    private readonly filmRepository: FilmRepository,
+    private readonly validationComposite: Validator
+  ) { }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const hasError = this.validationComposite.validate(httpRequest.params)
+      if (hasError) return badRequest(hasError)
+
       const { id } = httpRequest.params
       const film = await this.filmRepository.findOneById(id)
       if (!film) return badRequest(new FilmNotFoundError(id))
-      return success({ viewersAmount: film.viewersAmount })
+      return success({
+        spectatorsAmount: film.spectators.length,
+        spectators: film.spectators
+      })
     } catch (err) {
+      console.log(err)
       return serverError(new InternalServerError(err.stack))
     }
   }
