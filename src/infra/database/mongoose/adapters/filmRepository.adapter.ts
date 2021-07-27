16861aa36afc1e997a7b17a2465ca8dd65e7fdf5
Bruno Costa <b.brunocosta@outlook.com>
2021-07-26 23:20:44 -0300

    feat: add adapter implementation of FilmRepository

---
+++ src/infra/database/mongoose/adapters/filmRepository.adapter.ts
@@ -0,0 +1,33 @@
+import { FilmRepository } from '../../../../data/protocols/films/film.repository.protocol'
+import { Film } from '../../../../domain/film'
+import { mapFilm } from '../helpers/map'
+import { FilmModel } from '../models/films.model'
+class MongooseFilmRepositoryAdapter implements FilmRepository {
+  async addFilm ({ title, author, director }: Omit<Film, 'id' | 'viewersAmount' | 'spectators' | 'created-at'>): Promise<Film> {
+    const film = new FilmModel({ title, author, director })
+    const newFilm = await film.save()
+    return mapFilm(newFilm)
+  }
+
+  async getAll (): Promise<Film[] | []> {
+    const films = await FilmModel.find({})
+    return films.map(film => mapFilm(film))
+  }
+
+  async findOneByTitle (title: string): Promise<Film | null> {
+    const film = await FilmModel.findOne({ title }).exec()
+    return film ? mapFilm(film) : null
+  }
+
+  async findOneById (id: string): Promise<Film | null> {
+    const film = await FilmModel.findOne({ _id: id }).exec()
+    return film ? mapFilm(film) : null
+  }
+
+  async addSpectator (filmId: string, spectatorId: string): Promise<Film | null> {
+    const film = await FilmModel.findOneAndUpdate({ _id: filmId }, { $push: { spectators: spectatorId } }, { new: true }).exec()
+    return film ? mapFilm(film) : null
+  }
+}
+
+export default MongooseFilmRepositoryAdapter
