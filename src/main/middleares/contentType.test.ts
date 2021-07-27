6d6b20d3580b8c992ca869ed2454f3daa0aa91bf
Bruno Costa <b.brunocosta@outlook.com>
2021-07-26 23:51:53 -0300

    test: ensure returns xml content type when is forced

--- src/main/middleares/contentType.test.ts
+++ src/main/middleares/contentType.test.ts
@@ -8,4 +8,13 @@ describe('Content-Type Middleware', () => {
       .get('/test_content-type-json')
       .expect('content-type', /json/)
   })
+  test('Sould return xml content type when forced', async () => {
+    app.get('/test_content-type-xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+    await request(app)
+      .get('/test_content-type-xml')
+      .expect('content-type', /xml/)
+  })
 })
