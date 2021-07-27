7a252a3f2b4c07330d3ccb2e11a99bc4bcc0286e
Bruno Costa <b.brunocosta@outlook.com>
2021-07-27 04:42:10 -0300

    feat: add validation

--- src/presentation/controllers/films/addFilm.controller.ts
+++ src/presentation/controllers/films/addFilm.controller.ts
@@ -1,14 +1,22 @@
 import { FilmRepository } from '../../../data/protocols/films/film.repository.protocol'
 import FilmTitleAlreadyExistsError from '../../errors/filmTitleAlreadyExistsError'
 import InternalServerError from '../../errors/internalServerError'
-import { conflict, created, serverError } from '../../helpers/httpResponse.helper'
+import { badRequest, conflict, created, serverError } from '../../helpers/httpResponse.helper'
+import { Validator } from '../../helpers/validator/validator.protocol'
 import { Controller } from '../../protocols/controller.protocol'
 import { HttpRequest, HttpResponse } from '../../protocols/http.protocol'
 
 class AddFilmController implements Controller {
-  constructor (private readonly filmRepository: FilmRepository) {}
+  constructor (
+    private readonly filmRepository: FilmRepository,
+    private readonly validationComposite: Validator
+  ) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const error = this.validationComposite.validate(httpRequest.body)
+      if (error) return badRequest(error)
+
       const { title, author, director } = httpRequest.body
 
       const filmExists = await this.filmRepository.findOneByTitle(title)
